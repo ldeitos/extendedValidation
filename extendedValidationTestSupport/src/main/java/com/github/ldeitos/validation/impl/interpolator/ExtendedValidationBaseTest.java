3e2d277a06458780408662121b4de163f11e3659
Leandro Deitos <leandro.deitos@gmail.com>
2014-09-29 22:30:27 -0300

    Inclusão inicial.

---
+++ extendedValidationTestSupport/src/main/java/com/github/ldeitos/validation/impl/interpolator/ExtendedValidationBaseTest.java
@@ -0,0 +1,15 @@
+package com.github.ldeitos.validation.impl.interpolator;
+
+import org.jglue.cdiunit.AdditionalClasses;
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.runner.RunWith;
+
+import com.github.ldeitos.validation.impl.ValidatorImpl;
+
+@RunWith(CdiRunner.class)
+@AdditionalClasses({ValidatorImpl.class, MultipleBundlesSource.class})
+public class ExtendedValidationBaseTest {
+	
+	
+
+}
