03070331a1c5cbe72cdb7bfa56e086e4b155155b
Leandro Deitos <leandro.deitos@gmail.com>
2015-01-29 20:04:21 -0200

    Correção do marcador da severidade das violações

--- extendedValidationMultiModule/extendedValidation-core/src/main/java/com/github/ldeitos/validation/ConstraintSeverity.java
+++ extendedValidationMultiModule/extendedValidation-core/src/main/java/com/github/ldeitos/validation/ConstraintSeverity.java
@@ -3,38 +3,47 @@ package com.github.ldeitos.validation;
 import javax.validation.Payload;
 
 /**
- * 
+ *
  * @author <a href=mailto:leandro.deitos@gmail.com>Leandro Deitos</a>
  *
  */
 public class ConstraintSeverity implements Payload {
-	private class ERROR extends ConstraintSeverity {};
-	private class ALERT extends ConstraintSeverity {};
-	private class INFO extends ConstraintSeverity {};
-	private class WARN extends ConstraintSeverity {};
-	private class FATAL extends ConstraintSeverity {};
-	
-	public static Class<? extends ConstraintSeverity> error(){
+	public static final class ERROR extends ConstraintSeverity {
+	};
+
+	public static final class ALERT extends ConstraintSeverity {
+	};
+
+	public static final class INFO extends ConstraintSeverity {
+	};
+
+	public static final class WARN extends ConstraintSeverity {
+	};
+
+	public static final class FATAL extends ConstraintSeverity {
+	};
+
+	public static Class<? extends ConstraintSeverity> error() {
 		return ERROR.class;
 	}
-	
-	public static Class<? extends ConstraintSeverity> alert(){
+
+	public static Class<? extends ConstraintSeverity> alert() {
 		return ALERT.class;
 	}
-	
-	public static Class<? extends ConstraintSeverity> info(){
+
+	public static Class<? extends ConstraintSeverity> info() {
 		return INFO.class;
 	}
-	
-	public static  Class<? extends ConstraintSeverity> warn(){
+
+	public static Class<? extends ConstraintSeverity> warn() {
 		return WARN.class;
 	}
-	
-	public static  Class<? extends ConstraintSeverity> fatal(){
+
+	public static Class<? extends ConstraintSeverity> fatal() {
 		return FATAL.class;
 	}
-	
-	public static Class<? extends ConstraintSeverity> defaultValue(){
+
+	public static Class<? extends ConstraintSeverity> defaultValue() {
 		return error();
 	}
 }
