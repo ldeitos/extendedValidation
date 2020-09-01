1a1452d3ac34de16e0f01c2f47081969c150a3dd
00845212958 <jorge.rocha@serpro.gov.br>
2020-09-01 07:50:45 -0300

    Protegendo o formatador de mensagens do caracter dolar

---
+++ extendedValidation/src/test/java/com/github/ldeitos/validation/impl/util/PresentationMessageFormatterTest.java
@@ -0,0 +1,48 @@
+package com.github.ldeitos.validation.impl.util;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.enterprise.inject.Produces;
+
+import org.jglue.cdiunit.CdiRunner;
+import org.jglue.cdiunit.ProducesAlternative;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.github.ldeitos.validation.impl.configuration.ConfigInfoProvider;
+
+@RunWith(CdiRunner.class)
+@Ignore
+public class PresentationMessageFormatterTest {
+
+	@Produces
+	@ProducesAlternative
+	private ConfigInfoProvider cip = new ConfigInfoProvider() {
+		@Override
+		public String getConfigFileName() {
+			return "extendedValidation_configuredTemplateMessagePresentation.xml";
+		}
+
+		@Override
+		protected boolean isInTest() {
+			return true;
+		}
+	};
+
+	private static final String TEMPLATE = "{ER000}";
+	private static final String TEMPLATE_DOLAR = "{$$$}";
+
+	@Test
+	public void testFormatComDolarNaMensagem() {
+		assertEquals("(ER000) Erro: testando o dolar $ na mensagem.",
+				PresentationMessageFormatter.format(TEMPLATE, "Erro: testando o dolar $ na mensagem."));
+	}
+
+	@Test
+	public void testFormatComDolarNaChaveDaMensagem() {
+		assertEquals("($$$) Erro: testando o dolar na chave da mensagem.",
+				PresentationMessageFormatter.format(TEMPLATE_DOLAR, "Erro: testando o dolar na chave da mensagem."));
+	}
+
+}
